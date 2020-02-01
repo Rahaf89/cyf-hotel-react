27e5810c4b161c7b10fe5620d0ffb05a8d7f8e24
rahaf89 <rahaflh.900@hotmail.com>
2020-02-01 05:54:49 -0500

    ex.2

---
+++ src/Heading.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "react-dom";
+
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <img
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+        className="App-logo "
+      />{" "}
+      CYF Hotel
+    </header>
+  );
+};
+
+export default Heading;
